880f680eade2105a9ecea79ce18e3118e9aae14b
qwang06 <qiangsemail@gmail.com>
2012-09-22 23:34:54 -0400

    Added objects to corpus.js

--- js/corpus.js
+++ js/corpus.js
@@ -19,6 +19,7 @@ var templates = [
     "@verbs and @activities at @places with @firstname. It would be @adjective.",
     "@activities with @firstname at @places. If it is @adjective, then @verbs.",
 	"@verbs @adjective shit with @firstname at @places."
+	"Go to @places and take @adjective @objects to @firstname's house"
 ];
 
 var phrases = [
@@ -45,7 +46,8 @@ var labels = [
 corpus = {};
 
 corpus.firstname	 = ["Qiang","Gaurav","Peter","John"];
-corpus.activities	 = ["Play Board Games","Watch Jersey Shore","Surf","Table Tennis","Wrestle","Ski","Read 50 Shades of Grey","Skateboard","Chase Skirts","Set off Fireworks","Watch Twilight","Skinny Dipping","Scuba Diving","Punch a Tree","Take Nude Photos","Sky Dive","Streaking","Break Windows","Watch Porn"];
-corpus.places		 = ["The Park","School","The Library","The Sidewalk","The Highway","The Bus","Prisons","The Swimming Pool","The Beach","The Strip Club","Work","The Deli","The Supermarket","The Bookstore","The Bar","The Mall","Home"];
+corpus.activities	 = ["Play Board Games","Watch Jersey Shore","Surf","Table Tennis","Wrestle","Ski","Read 50 Shades of Grey","Skateboard","Chase Skirts","Set off Fireworks","Watch Twilight","Skinny Dip","Scuba Dive","Punch a Tree","Take Nude Photos","Sky Dive","Streaking","Break Windows","Watch Porn"];
+corpus.places		 = ["The Park","School","The Library","The Sidewalk","The Highway","The Bus","Prisons","The Swimming Pool","The Beach","The Strip Club","Work","The Deli","The Supermarket","The Bookstore","The Bar","The Mall","Home","The Bathroom","The Bed","Your Parent's Bathroom"];
 corpus.verbs		 = ["Clean","Grow","Destroy","Shout","Sing","Dance","Get High","Fuck","Drink","Fight","Drive","Grind","Shake your ass","Slit throats","Run","Sweat","Throw Shit","Vomit"];
-corpus.adjective	 = ["Scary","Evil","Awesome","Wild","Crazy","Hilarious","Cruel","Fucked up","Silly","Shitty","Attractive","Huge"];
+corpus.adjective	 = ["Scary","Evil","Awesome","Wild","Crazy","Hilarious","Cruel","Fucked up","Silly","Shitty","Attractive","Huge","Heavenly","Mindless","Graceful","Agonizing","Cynical","Dashing","Earsplitting"];
+corpus.objects		 = ["Skateboards","Chairs","Computers","Books","Boots","Dresses","Skirts","Togas","Spears","Swords","Knives","Flamethowers","AK-47s","Pistols","Flowers","Puppies","Cats","Chemicals","Drugs"];
