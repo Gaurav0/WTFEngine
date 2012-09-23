f32e7f5e958c770ecf7d82b008c7f2e56d7873cf
qwang06 <qiangsemail@gmail.com>
2012-09-22 23:14:18 -0400

    Changed corpus.js

--- js/corpus.js
+++ js/corpus.js
@@ -18,14 +18,15 @@ var templates = [
 	"Go @activities with @firstname at @places. Then @verbs until it is @adjective.",
     "@verbs and @activities at @places with @firstname. It would be @adjective.",
     "@activities with @firstname at @places. If it is @adjective, then @verbs."
+	"@verbs @adjectives shit with @firstname at @places."
 ];
 
 var phrases = [
 	"It's a fucking beautiful day! Why not",
 	"No need to be fucking bored out of your mind. You could",
-	"Miss fucking your ex? Then why not just",
-	"Feeling fucking down? Pick yourself up and",
-	"Not the fucking life of the party? Forget it and",
+	"Don't just fucking sit there",
+	"Feeling down? Pick your sorry self up and",
+	"Not the life of the party? Fucking forget it and",
     "Forget all your fucking problems. Relax and"
 ];
 
@@ -45,6 +46,6 @@ corpus = {};
 
 corpus.firstname	 = ["Qiang","Gaurav","Peter","John"];
 corpus.activities	 = ["Play Board Games","Watch Jersey Shore","Surf","Table Tennis","Wrestle","Ski","Read 50 Shades of Grey","Skateboard","Chase Skirts","Set off Fireworks","Watch Twilight","Skinny Dipping","Scuba Diving","Punch a Tree","Take Nude Photos","Sky Dive","Streaking","Break Windows","Watch Porn"];
-corpus.places		 = ["The Park","School","The Library","The Sidewalk","The Highway","The Bus","Prison","The Swimming Pool","The Beach","The Strip Club","Work","The Deli","The Supermarket","The Bookstore","The Bar","The Mall","Home"];
+corpus.places		 = ["The Park","School","The Library","The Sidewalk","The Highway","The Bus","Prisons","The Swimming Pool","The Beach","The Strip Club","Work","The Deli","The Supermarket","The Bookstore","The Bar","The Mall","Home"];
 corpus.verbs		 = ["Clean","Grow","Destroy","Shout","Sing","Dance","Get High","Fuck","Drink","Fight","Drive","Grind","Shake your ass","Slit throats","Run","Sweat","Throw Shit","Vomit"];
 corpus.adjective	 = ["Scary","Evil","Awesome","Wild","Crazy","Hilarious","Cruel","Fucked up","Silly","Shitty","Attractive","Huge"];
